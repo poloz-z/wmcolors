50161071fdb02056bc3997a988a9d5d80f1eea30
Spaghetti <alejandrolpz0619@gmail.com>
2022-05-02 16:39:29 -0400

    ADD: validate_json function created & json_schema.
    MINOR FIX: fixed some typos in test.json

---
+++ json_parser.py
@@ -0,0 +1,56 @@
+"""
+Project under GPLv3
+Repository: https://github.com/poloz-z/wmcolors
+
+"""
+# Imports.
+import json
+import jsonschema
+from jsonschema import validate
+
+"""
+This function loads the given schema at the root folder.
+"""
+def get_schema():
+    with open('json_schema.json', 'r') as file:
+        schema = json.load(file)
+    return schema
+
+"""
+Function to load the test.json at doc/test.json
+"""
+def get_test():
+  with open('./doc/test.json', 'r') as test:
+    test = json.load(test)
+  return test
+
+"""
+Function to validate the json against 
+"""
+def validate_json(json_data):
+    """REF: https://json-schema.org/ """
+    # Describe what kind of json you expect.
+    execute_api_schema = get_schema()
+    # Gets the testing json
+    json_for_testing = get_test()
+
+    try:
+        validate(
+          instance=json_for_testing, 
+          schema=execute_api_schema
+          )
+    except jsonschema.exceptions.ValidationError as err:
+        print(err)
+        err = "Given JSON data is InValid"
+        return False, err
+
+    message = "Given JSON data is Valid"
+    return True, message
+
+
+# Convert json to python object.
+jsonData = json.loads('{"id" : 10,"name": "DonOfDen","contact_number":1234567890}')
+
+# validate it
+is_valid, msg = validate_json(jsonData)
+print(msg)
