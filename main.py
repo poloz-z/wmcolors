33c35b65a978266096665ecb71de7e6be623450a
poloz-z <jpolo5678@gmail.com>
2022-05-07 22:43:06 -0400

    main en desarrollo...

---
+++ main.py
@@ -0,0 +1,19 @@
+import json, os, sys
+
+def dicc_json():
+    with open("./dot/test.json", "r") as x:
+        dicc_themes = json.load(x)
+
+    return dicc_themes
+
+class WMColors():
+    """Clase maestra o principal nose como se decia"""
+    def 
+    
+
+if __name__ == "__main__":
+    try: 
+        with open("./doc/test.json", "r") as x:
+            dicc_themes = json.load(x)
+
+    exec 
